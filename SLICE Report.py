3d676939e1f3d39d6971b82e604db5df87e08763
Bossa-Antiga <nnrt.int@gmail.com>
2026-02-06 16:56:42 +0900

    Update SLICE Report.py

--- SLICE Report.py
+++ SLICE Report.py
@@ -154,8 +154,7 @@ for photo in photos:
 
 with open(file_path, "wb") as f:
                                f.write(photo.getbuffer())
-
-                    photo_names.append(filename)
+                               photo_names.append(filename)
 
                 daily_data[f"day_{day}"] = {
                     "enthusiasm": enthusiasm,
