a280edb0bd557e1b130d8a59217f4ab3a55dafdd
Bossa-Antiga <nnrt.int@gmail.com>
2026-02-11 21:06:28 +0900

    Update SLICE Report.py

--- SLICE Report.py
+++ SLICE Report.py
@@ -98,13 +98,13 @@ for school_index, school_tab in enumerate(school_tabs, start=1):
                 # Color Indicator
                 if enthusiasm == "Low":
                     color = "#ff4b4b"
-                    text = "Low enthusiasm 😕"
+                    text = "😕"
                 elif enthusiasm == "Average":
                     color = "#f7d046"
-                    text = "Average enthusiasm 😐"
+                    text = "😐"
                 else:
                     color = "#2ecc71"
-                    text = "High enthusiasm 😄"
+                    text = "😄"
 
                 st.markdown(
                     f"""
