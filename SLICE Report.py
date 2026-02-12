b5d4bc683ca20aa63eb98e1072456310b857106c
Bossa-Antiga <nnrt.int@gmail.com>
2026-02-12 15:57:24 +0900

    Update SLICE Report.py

--- SLICE Report.py
+++ SLICE Report.py
@@ -209,7 +209,7 @@ for school_index, school_tab in enumerate(school_tabs, start=1):
                         st.session_state[confirm_key] = False
                         st.rerun()
 
-if st.session_state[confirm_key]:
+ if st.session_state[confirm_key]:
 
      if not school_name or not teacher_name:
         st.error("School name and teacher are required.")
